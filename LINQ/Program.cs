a4e8230e98572211d4bfa84264132119d958f28b
derrick thomas <avirajoesph@yahoo.com>
2024-06-19 11:59:00 -0500

    fixed project

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            List<string> stringList = new List<string>();
+            stringList.Add("GTA5");
+            stringList.Add("Uncharted 4");
+            stringList.Add("Fortnite");
+            stringList.Add("Call Of Duty");
+
+            stringList.OrderBy(s => s.Length).ToList().ForEach(x =>Console.WriteLine(x) );
+            
+            
+                
+            
         }
+
     }
 }
